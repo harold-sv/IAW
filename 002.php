d0c8927a9598aaad187c8025fcaeaae9dc648396
Harold Sanchez <hd.sanchezvargas@gmail.com>
2026-01-04 21:04:19 +0100

    subtotal del carrito

--- 002.php
+++ 002.php
@@ -8,4 +8,12 @@ $carrito = [
     ["producto" => "Teclado", "precio" => 45, "cantidad" => 1],
 ];
 
+// 1. Mostramos el contenido del carrito con subtotal por producto.
+foreach ($carrito as $item) {
+    $subtotal = $item['precio'] * $item['cantidad'];
+    
+    echo "Producto: <p>{$item['producto']}</p> <br>";
+    echo "Precio unitario: {$item['precio']}€ | Cantidad: {$item['cantidad']} <br>";
+    echo "Subtotal: <p>$subtotal €</p> <hr>";
+}
 ?>
