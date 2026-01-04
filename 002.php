8a7ea9cafbd8436dc4a8aa559700c922282f2757
Harold Sanchez <hd.sanchezvargas@gmail.com>
2026-01-04 20:52:14 +0100

    init carrito de compra, array

---
+++ 002.php
@@ -0,0 +1,11 @@
+<?php
+// CARRITO DE COMPRAS
+
+// Array asociativo del producto.
+$carrito = [
+    ["producto" => "Portátil", "precio" => 1200, "cantidad" => 1],
+    ["producto" => "Ratón", "precio" => 25, "cantidad" => 2],
+    ["producto" => "Teclado", "precio" => 45, "cantidad" => 1],
+];
+
+?>
