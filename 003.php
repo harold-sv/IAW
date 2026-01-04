73caf13beb6f00381b9fd8c08e1cab2a4a494c6f
Harold Sanchez <hd.sanchezvargas@gmail.com>
2026-01-04 22:10:14 +0100

    init cambio de texto a poesia

---
+++ 003.php
@@ -0,0 +1,16 @@
+<?php
+
+// cambiamos texto a (La poesía) y definimos la variable
+$texto = "Oda al Elefante de la Web
+Entre etiquetas de menor y mayor, nace un script con alma de servidor.
+Con un signo de $ por bandera, guardas datos de cualquier manera.
+<?php es tu grito de entrada, la señal de una web bien orquestada.
+No importa si es un String o un Entero, tú procesas el código el mundo entero.
+En el foreach bailan los estudiantes, en el carrito sumas los diamantes.
+Con Apache y XAMPP como aliados, los errores de sintaxis son olvidados.
+Aunque digan que el tiempo te ha pasado, sigues firme, robusto y bien plantado.
+Porque si WordPress y la web aún resisten, es porque tus funciones aún existen. 
+Terminas con ?> y cierras el telón, dejando un HTML en perfecta unión. PHP, mi fiel y viejo compañero, el lenguaje más noble y aventurero.";
+
+
+?>
