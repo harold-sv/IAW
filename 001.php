217463ffebc768b761664ef4dbbe07ac6d01a4f6
Harold Sanchez <hd.sanchezvargas@gmail.com>
2026-01-04 19:05:29 +0100

    crear funcion para calcular promedio

--- 001.php
+++ 001.php
@@ -1,2 +1,15 @@
 <?php
 // Gestor de notas de Estudiantes:
+
+// Creamos el array:
+$estudiantes = [
+    "Ana" => [8, 7, 9],
+    "Luis" => [5, 6, 4],
+    "María" => [10, 9, 10],
+    "Carlos" => [6, 6, 6]
+];
+// Función para calcular el promedio de notas:
+function calcularPromedio($notas) {
+    $suma = array_sum($notas);
+    return $suma / count($notas);
+}
